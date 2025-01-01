56c9e972f3beacfab6fc8070b8aab7cfaf845849
MetaMsa <mserhataslan@hotmail.com>
2025-01-02 00:45:03 +0300

    main

--- db.php
+++ db.php
@@ -6,5 +6,5 @@
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
-    $conn = new PDO("mysql:host=$_ENV[DE_MYSQL_DB_HOST];dbname=$_ENV[DE_MYSQL_DB_NAME]", $_ENV['DE_MYSQL_DB_USER_NAME'], $_ENV['DE_MYSQL_DB_PASSWORD']);
+    $conn = new PDO("mysql:host=localhost;dbname=if0_37485059_odev", "root", );
 ?>
